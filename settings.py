032b3f651693f540572ddbcf5f7e66f5de021004
arya <xxb9611@outlook.com>
2023-07-25 00:12:59 +0800

    [update] 配置项优化

--- settings.py
+++ settings.py
@@ -1,39 +1,49 @@
 import os
+from functools import cache
 import dotenv
+from fastapi import FastAPI
 from tortoise.contrib.fastapi import register_tortoise
 
-# 读取.env文件数据
-dotenv.load_dotenv()
 
-# 数据库设置
-DB_ORM_CONFIG = {
-    "connections": {
-        "pgsql": {
-            'engine': 'tortoise.backends.asyncpg',
-            "credentials": {
-                'host': os.getenv('BASE_HOST', '127.0.0.1'),
-                'user': os.getenv('BASE_USER', 'arya'),
-                'password': os.getenv('BASE_PASSWORD', '12345678'),
-                'port': os.getenv('BASE_PORT', 5432),
-                'database': os.getenv('BASE_DB', 'postgres'),
-            }
-        },
-    },
-    "apps": {
-        "my_app": {"models": ["User.models"], "default_connection": "pgsql"},
-        # "db2": {"models": ["models.db2"], "default_connection": "db2"},
-        # "db3": {"models": ["models.db3"], "default_connection": "db3"}
-    },
-    'use_tz': False,
-    'timezone': 'Asia/Shanghai',
-    "generate_schemas": True
-}
+@cache
+class Setting:
+    def __init__(self):
+        # 读取.env文件数据
+        dotenv.load_dotenv()
+
+    @property
+    def db_rom_config(self):
+        return {
+            "connections": {
+                "pgsql": {
+                    'engine': 'tortoise.backends.asyncpg',
+                    "credentials": {
+                        'host': os.getenv('BASE_HOST', '127.0.0.1'),
+                        'user': os.getenv('BASE_USER', 'arya'),
+                        'password': os.getenv('BASE_PASSWORD', '12345678'),
+                        'port': os.getenv('BASE_PORT', 5432),
+                        'database': os.getenv('BASE_DB', 'postgres'),
+                    }
+                },
+            },
+            "apps": {
+                "User": {"models": ["User.models"], "default_connection": "pgsql"},
+                # "db2": {"models": ["models.db2"], "default_connection": "db2"},
+                # "db3": {"models": ["models.db3"], "default_connection": "db3"}
+            },
+            'use_tz': False,
+            'timezone': 'Asia/Shanghai',
+            "generate_schemas": True
+        }
 
 
-def link_db(app):
+setting = Setting()
+
+
+def link_db(app: FastAPI):
     register_tortoise(
         app,
-        config=DB_ORM_CONFIG,
+        config=setting.db_rom_config,
         generate_schemas=True,
         add_exception_handlers=False,
     )
